21da98acc413c9ace4ad31ffe77bc7e29024fd5c
hammerorz <16081812@hdu.edu.cn>
2018-01-02 22:21:12 +0800

    update 2018-01-02

---
+++ geometry.go
@@ -0,0 +1,39 @@
+package main
+
+import "math"
+import "fmt"
+
+type Point struct{ X, Y float64 }
+type Path []Point
+
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
+
+func main() {
+	p := Point{1, 2}
+	q := Point{4, 6}
+	fmt.Println(Distance(p, q))
+	fmt.Println(p.Distance(q))
+	perim := Path{
+		{1, 1},
+		{5, 1},
+		{5, 4},
+		{1, 1},
+	}
+	fmt.Println(perim.Distance())
+}
