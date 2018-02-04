e9f5811efa0a32a15c253a154415df3b7934d057
hammerorz <16081812@hdu.edu.cn>
2018-02-04 14:59:47 +0800

    update 2018-02-04

---
+++ ch7/sorting/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"text/tabwriter"
+	"time"
+)
+
+type Track struct {
+	Title   string
+	Artisan string
+	Album   string
+	Year    int
+	Length  time.Duration
+}
+
+var track = []*Track{
+	{"GO", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+	{"GO", "Moby", "Moby", 1992, length("3m37s")},
+	{"GO Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+	{"Ready 2 GO", "Martin Solveig", "Smash", 2011, length("4m24s")},
+}
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
+
+func printTracks(tracks []*Track) {
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
+	for _, t := range tracks {
+		fmt.Fprintf(tw, format, t.Title, t.Artisan, t.Album, t.Year, t.Length)
+	}
+	tw.Flush()
+}
+
+type byArtist []*Track
+
+func (x byArtist) Len() int           { return len(x) }
+func (x byArtist) Less(i, j int) bool { return x[i].Artisan < x[j].Artisan }
+func (x byArtist) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type customSort struct {
+	t    []*Track
+	less func(x, y *Track) bool
+}
+
+func (x customSort) Len() int           { return len(x.t) }
+func (x customSort) Less(i, j int) bool { return x.less(x.t[i], x.t[j]) }
+func (x customSort) Swap(i, j int)      { x.t[i], x.t[j] = x.t[j], x.t[i] }
+
+func main() {
+	//sort.Sort(byArtist(track))
+
+	sort.Sort(customSort{track, func(x, y *Track) bool {
+		if x.Title != y.Title {
+			return x.Title < y.Title
+		}
+		if x.Year != y.Year {
+			return x.Year < y.Year
+		}
+		if x.Length != y.Length {
+			return x.Length < y.Length
+		}
+		return false
+	}})
+	printTracks(track)
+}
