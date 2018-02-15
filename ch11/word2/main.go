58cb4097fa2ad6827cdcfa28b91129dd012bb016
hammerorz <16081812@hdu.edu.cn>
2018-02-15 16:54:28 +0800

    update 2018-02-15

---
+++ ch11/word2/main.go
@@ -0,0 +1,18 @@
+package word
+
+import "unicode"
+
+func IsPalindrome(s string) bool {
+	letters := make([]rune, 0, len(s))
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			letters = append(letters, unicode.ToLower(r))
+		}
+	}
+	for i := range letters {
+		if letters[i] != letters[len(letters)-1-i] {
+			return false
+		}
+	}
+	return true
+}
