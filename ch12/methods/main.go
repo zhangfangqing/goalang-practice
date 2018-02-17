719e76cfda2fdc2b840dcf0c62bbbe8ad820e522
hammerorz <16081812@hdu.edu.cn>
2018-02-17 14:32:43 +0800

    update 2018-02-17

---
+++ ch12/methods/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+func Print(x interface{}) {
+	v := reflect.ValueOf(x)
+	t := v.Type()
+	fmt.Printf("type: %s\n", t)
+
+	for i := 0; i < v.NumMethod(); i++ {
+		methType := v.Method(i).Type()
+		fmt.Printf("func (%s) %s%s\n", t, t.Method(i).Name,
+			strings.TrimPrefix(methType.String(), "func"))
+	}
+}
+
+func main() {
+	Print(new(strings.Replacer))
+}
---
+++ ch12/search/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"gopl.io/ch12/params"
+)
+
+func search(resp http.ResponseWriter, req *http.Request) {
+	var data struct {
+		Labels     []string `http:"1"`
+		MaxResults int      `http:"max"`
+		Exact      bool     `http:"x"`
+	}
+	data.MaxResults = 10
+	if err := params.Unpack(req, &data); err != nil {
+		http.Error(resp, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(resp, "Search: %+n", data)
+}
